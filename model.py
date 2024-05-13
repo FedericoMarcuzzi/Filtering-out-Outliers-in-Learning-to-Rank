6596b83858436ef32adc80b13010a1590f6fac77
federicomarcuzzi <federico.marcuzzi@unive.it>
2024-05-13 15:22:41 +0200

    curr sour

--- model.py
+++ model.py
@@ -14,36 +14,56 @@ class SOUR():
         self.eval_group = eval_group
         self.eval_names = eval_names
 
-    def train(self, params, outliers_type, start, end, p_sour=1, last_sour=False, cutoff=None, min_neg_rel=0, idx_to_removed=None, **kwargs):
-        if idx_to_removed is None:
-            if cutoff is None:
-                cutoff = params['eval_at']
-            
-            cleaned_params = rename_dict_key(params, "num_iterations", ["num_iterations", "num_iteration", "n_iter", "num_tree", "num_trees", "num_round", "num_rounds", "nrounds", "num_boost_round", "n_estimators", "max_iter"])
-            cleaned_params = rename_dict_key(params, "early_stopping_round", ["early_stopping_rounds", "early_stopping", "n_iter_no_change"])
+    def train(self, params, outliers_type, start, end, p_sour=1, last_sour=False, cutoff=None, min_neg_rel=0, **kwargs):
+        if cutoff is None:
+            cutoff = params['eval_at']
 
-            outliers_finder = OutliersFinder(outliers_type, start, end, cutoff, min_neg_rel)
-            train_set, valid_sets, valid_names = prepare_lightgbm_sets((self.queries, self.labels, self.qs_len), include_train=True)
+        is_curr = True
+        if type(end) is not list:
+            end = [end]
+            is_curr = False
+        
+        cleaned_params = rename_dict_key(params, "num_iterations", ["num_iterations", "num_iteration", "n_iter", "num_tree", "num_trees", "num_round", "num_rounds", "nrounds", "num_boost_round", "n_estimators", "max_iter"])
+        cleaned_params = rename_dict_key(params, "early_stopping_round", ["early_stopping_rounds", "early_stopping", "n_iter_no_change"])
 
-            save_early_stopping_rounds = 0
-            save_num_iterations = 100
+        outliers_finder = OutliersFinder(outliers_type, start, end, cutoff, min_neg_rel)
+        train_set, valid_sets, valid_names = prepare_lightgbm_sets((self.queries, self.labels, self.qs_len), include_train=True)
 
-            if "early_stopping_rounds" in cleaned_params:
-                save_early_stopping_rounds = cleaned_params["early_stopping_rounds"]
-            if "num_iterations" in cleaned_params:
-                save_num_iterations = cleaned_params["num_iterations"]
+        save_early_stopping_rounds = 0
+        save_num_iterations = 100
 
-            cleaned_params["early_stopping_rounds"] = 0
-            cleaned_params["num_iterations"] = end
-            
-            model = lgb.train(cleaned_params, train_set, valid_sets=valid_sets, valid_names=valid_names, feval=outliers_finder)
+        if "early_stopping_rounds" in cleaned_params:
+            save_early_stopping_rounds = cleaned_params["early_stopping_rounds"]
+        if "num_iterations" in cleaned_params:
+            save_num_iterations = cleaned_params["num_iterations"]
 
-            cleaned_params["early_stopping_rounds"] = save_early_stopping_rounds
-            cleaned_params["num_iterations"] = save_num_iterations
+        cleaned_params["early_stopping_rounds"] = 0
+        cleaned_params["num_iterations"] = end[-1]
+        
+        model = lgb.train(cleaned_params, train_set, valid_sets=valid_sets, valid_names=valid_names, feval=outliers_finder)
 
-            idx_to_removed = outliers_finder.get_outliers_ids(self, p_sour=p_sour, last_sour=last_sour)
+        cleaned_params["early_stopping_rounds"] = save_early_stopping_rounds
+        cleaned_params["num_iterations"] = save_num_iterations
 
         clean_queries, clean_labels, clean_qs_lens = remove_docs(self.queries, self.labels, self.qs_len, idx_to_removed)
         valid_sets, valid_names = prepare_lightgbm_sets((clean_queries, clean_labels, clean_qs_lens), [self.queries, self.labels, self.qs_len, self.eval_names])
-        model = lgb.train(params, train_set, valid_sets=valid_sets, valid_names=valid_names, **kwargs)
+        
+        model = None
+        if "init_model" in kwargs:
+            model = kwargs.pop("init_model")
+
+        pre_end = 0
+        for i, idx_to_removed in enumerate(outliers_finder.get_outliers_ids(self, p_sour=p_sour, last_sour=last_sour)):
+            clean_queries, clean_labels, clean_qs_lens = remove_docs(self.queries, self.labels, self.qs_len, idx_to_removed)
+            valid_sets, valid_names = prepare_lightgbm_sets((clean_queries, clean_labels, clean_qs_lens), [self.queries, self.labels, self.qs_len, self.eval_names])        
+            
+            if is_curr:
+                if i == len(end) - 1:
+                    cleaned_params["num_iterations"] = save_num_iterations - pre_end if save_num_iterations > pre_end else save_num_iterations, 
+                else:
+                    cleaned_params["num_iterations"] = (end[i] - pre_end)
+                    pre_end = end[i]
+
+            model = lgb.train(cleaned_params, train_set, valid_sets=valid_sets, valid_names=valid_names, init_model=model, **kwargs)
+
         return model
--- outliers_detector.py
+++ outliers_detector.py
@@ -8,19 +8,28 @@ class OutliersFinder():
         self.cutoff = cutoff
         self.min_neg_rel = min_neg_rel # minimal doc label to consider a document as not-relevant
 
-        self.all_outleirs_ids = None
+        self.all_outleirs_ids = [None] * len(end)
         self.last_outleirs_ids = None
         self.iteration = 0
+        self.curr_end = 0
 
         if self.outliers_type not in ["neg", "pos", "all"]:
             raise "<outliers_type> must by 'neg', 'pos', or 'all'"
 
     def __call__(self, y_score, data):
-        if self.iteration >= self.start and data.params['name'] == 'train_set':
-            if self.all_outleirs_ids is None:
-                self.all_outleirs_ids = np.zeros(y_score.shape[0])
-            self.last_outleirs_ids = self._compute_outliers_ids(data.label, y_score, data.group, self.cutoff)
-            self.all_outleirs_ids[self.last_outleirs_ids] += 1
+        if self.curr_end < len(self.end):
+            if self.iteration < self.end[self.curr_end]:
+                if self.iteration >= self.start and data.params['name'] == 'train_set':
+                    if self.all_outleirs_ids[self.curr_end] is None:
+                        if self.curr_end == 0:
+                            self.all_outleirs_ids[self.curr_end] = np.zeros(y_score.shape)
+                        else:
+                            self.all_outleirs_ids[self.curr_end] = np.copy(self.all_outleirs_ids[self.curr_end - 1])
+
+                    self.last_outleirs_ids = self._compute_outliers_ids(data.label, y_score, data.group, self.cutoff)
+                    self.all_outleirs_ids[self.curr_end][self.last_outleirs_ids] += 1
+            else:
+                self.curr_end += 1
 
         self.iteration += 1
         return '', 1, True
@@ -56,11 +65,21 @@ class OutliersFinder():
         return np.concatenate(ids_list)
     
     # gets consistent / frequent / last iteration outlier documents' IDs in the interval [start, end). NOTE: counting from zero.   
-    def get_outliers_ids(self, p_sour=1, last_sour=False):
+    def get_outliers_ids(self, p_sour=1, last_sour=False, curr_sour=False):
         if last_sour:
             return self.last_outleirs_ids
         
-        return np.where(self.all_outleirs_ids >= int(p_sour * (self.end - self.start)))[0]
+        if not curr_sour:
+            return np.where(self.all_outleirs_ids[i] >= int(p_sour * (self.end[0] - self.start)))[0]
+        
+        empty_docs_idx = np.array([], dtype=int)
+        idx_to_remove = np.zeros(self.all_outleirs_ids[0].shape)
+        list_outleirs_ids = [np.where(self.all_outleirs_ids[i] >= int(p_sour * (end - self.start)))[0] for i, end in enumerate(self.end)]
+        list_outleirs_ids.append(empty_docs_idx)
+
+        for i, idx in enumerate(list_outleirs_ids):
+            idx_to_remove[idx] += 1
+            yield np.where(idx_to_remove > i)[0]
     
     def get_outliers_ids_from_score(self, y_true, y_score, qs_len):
         return self._compute_outliers_ids(self, y_true, y_score, qs_len)
